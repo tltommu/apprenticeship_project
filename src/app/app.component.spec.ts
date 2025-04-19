674a3fe89ba2c4988b12f88e3da4a0c90e393f08
tltommu <tltommu527@gmail.com>
2025-04-19 21:36:19 +0800

    changing test and adding email validation message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,10 +21,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Apprenticeship_Project');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Apprenticeship_Project');
-  });
+  
 });
